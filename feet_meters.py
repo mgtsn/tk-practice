6027e99100a1623da042cfe2ff797d0a43d76568
Morgan <morgantosini@gmail.com>
2021-09-12 10:30:52 -0400

    Calculation working

---
+++ feet_meters.py
@@ -0,0 +1,33 @@
+from tkinter import *
+from tkinter import ttk
+
+
+def calculate():
+    try:
+        val = float(feet.get())
+        meters.set(int(0.3408 * val * 10000.0 + 0.5) / 10000)
+    except ValueError:
+        pass
+
+
+root = Tk()
+root.title("FTM Calculator")
+
+frame = ttk.Frame(root, padding="3 3 12 12")
+frame.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+
+feet = StringVar()
+feet_entry = ttk.Entry(frame, width=8, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=(W, E))
+ttk.Label(frame, text="Feet").grid(column=3, row=1, sticky=W)
+
+meters = StringVar()
+ttk.Label(frame, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+ttk.Label(frame, text="Meters").grid(column=3, row=2, sticky=W)
+
+ttk.Button(frame, text="Calculate", command=calculate).grid(column=3, row=3, sticky=E)
+
+root.mainloop()
